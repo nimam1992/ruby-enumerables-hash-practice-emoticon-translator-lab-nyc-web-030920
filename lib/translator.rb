827751103be8466cac439b97954cf5e6b8c7520a
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-13 19:28:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file)
+meaning = {}
+emotions = {}
+emoticons = YAML.load_file(file)
+emoticons.each do |(word,value)|
+  meaning[value[1]] = word
+  emotions[value[0]] = value[1]
+end
+emoticons_hash =
+  {:get_meaning => meaning,
+   :get_emoticon => emotions
+  }
+emoticons_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file,emoticon)
+  emoticon_lib = load_library(file)
 end
 
 def get_english_meaning
   # code goes here
 end
+
+p load_library("./lib/emoticons.yml")
